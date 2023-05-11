b288d028f224397a8f1b4500a098755dba4293f1
santakratos <santakratos10@gmail.com>
2023-05-11 22:14:49 +0300

    Added netflix logo v0.1 and fixed bg login page v0.1.3

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 const Login = () => {
     return (
@@ -11,6 +12,12 @@ const Login = () => {
             <img
                 src="https://rb.gy/p2hphi"
                 className="-z-10 !hidden opacity-60 sm:!inline w-full h-full object-cover"
+            />
+            <img
+                src="https://rb.gy/ulxxee"
+                className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+                width={150}
+                height={150}
             />
         </div>
     );
