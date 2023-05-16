efcc68f8a689a64cdd6f11346600aeee6e824119
santakratos <santakratos10@gmail.com>
2023-05-16 23:22:00 +0300

    Added logout v0.1 and added title at home page v0.1

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -9,15 +9,18 @@ interface Props {
 
 const Thumbnail = ({movie}: Props) => {
     return (
-        <div className='relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105'>
-            <Image
-                src={`https://image.tmdb.org/t/p/w500${
-                    movie.backdrop_path || movie.poster_path
-                }`}
-                className="rounded-sm object-cover md:rounded"
-                layout="fill"
-                alt=''
-            />
+        <div className='h-[170px] cursor-pointer md:hover:scale-105 transition duration-200 ease-out'>
+            <div className='relative  min-w-[180px] md:h-36 md:min-w-[260px] '>
+                <Image
+                    src={`https://image.tmdb.org/t/p/w500${
+                        movie.backdrop_path || movie.poster_path
+                    }`}
+                    className="rounded-sm object-cover md:rounded"
+                    layout="fill"
+                    alt=''
+                />
+            </div>
+            <h2 className='pt-1 text-white text-center'>{movie.title || movie.name}</h2>
         </div>
 );
 };
